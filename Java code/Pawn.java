3978f15b6a5e7caa4500611154566e46b3df1722
Ioana Marin <61522769+IoanaMarin1106@users.noreply.github.com>
2020-03-07 23:10:28 +0200

    Update Pawn.java

--- Java code/Pawn.java
+++ Java code/Pawn.java
@@ -3,13 +3,13 @@ public final class Pawn extends Piece {
 		long src = move[0], dest = move[1];
 
 		if((dest & blackPieces) != 0) {// attack
-			if((src << 7) == dest || (src << 9) == dest) {
+			if((src << 7L) == dest || (src << 9L) == dest) {
 				return true;
 			}
 		} else {// avans
-			if(dest == (src << 8)) {
+			if(dest == (src << 8L)) {
 				return true;
-			} else if(dest == (src << 16) && (src >= (1L << 8) && src < (1L << 16))) {
+			} else if(dest == (src << 16L) && (src >= (1L << 8L) && src < (1L << 16L))) {
 				return true;
 			}
 		}
@@ -21,13 +21,13 @@ public final class Pawn extends Piece {
 		long src = move[0], dest = move[1];
 
 		if((dest & whitePieces) != 0) {
-			if((src >> 7) == dest || (src >> 9) == dest) {
+			if((src >> 7L) == dest || (src >> 9L) == dest) {
 				return true;
 			}
 		} else {
-			if(dest == (src >> 8)) {
+			if(dest == (src >> 8L)) {
 				return true;
-			} else if(dest == (src >> 16) && (src >= (1L << 48) && src < (1L << 56))) {
+			} else if(dest == (src >> 16L) && (src >= (1L << 48L) && src < (1L << 56L))) {
 				return true;
 			}
 		}
