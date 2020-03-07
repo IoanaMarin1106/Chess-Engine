7464d2bcacd28611e627af406f86699990ab026f
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-07 21:58:12 +0200

    updated King's isValidMove method

--- Java code/King.java
+++ Java code/King.java
@@ -2,12 +2,13 @@ public final class King extends Piece {
 	public static boolean isValidMove(long[] move) {
 		long src = move[0], dest = move[1];
 		
-		if(src << 8 == dest || src << 9 == dest || src << 7 == dest ||
-			src >> 8 == dest || src >> 9 == dest || src >> 7 == dest ||
-			src << 1 == dest || src >> 1 == dest) {
-			return true;
-		}		
-
+		if(src << 9 == dest) return true;
+		if(src << 8 == dest) return true;
+		if(src << 7 == dest) return true;
+		if(src >> 9 == dest) return true;
+		if(src >> 8 == dest) return true;
+		if(src >> 7 == dest) return true;
+		
 		return false;
 	}
 }
