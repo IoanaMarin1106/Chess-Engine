956b987ebc54153c10dea4228fa46ebaa36dc43c
Ioana Marin <61522769+IoanaMarin1106@users.noreply.github.com>
2020-03-07 23:08:59 +0200

    Update Knight.java

--- Java code/Knight.java
+++ Java code/Knight.java
@@ -2,14 +2,14 @@ public final class Knight extends Piece {
 	public static boolean isValidMove(long[] move) {
 		long src = move[0], dest = move[1];
 
-		if((src << 6) == dest)	return true;
-		if((src << 10) == dest)	return true;
-		if((src << 15) == dest) return true;
-		if((src << 17) == dest)	return true;
-		if((src >> 6) == dest)	return true;
-		if((src >> 10) == dest)	return true;
-		if((src >> 15) == dest)	return true;
-		if((src >> 17) == dest)	return true;
+		if((src << 6L) == dest)		return true;
+		if((src << 10L) == dest)	return true;
+		if((src << 15L) == dest) 	return true;
+		if((src << 17L) == dest)	return true;
+		if((src >> 6L) == dest)		return true;
+		if((src >> 10L) == dest)	return true;
+		if((src >> 15L) == dest)	return true;
+		if((src >> 17L) == dest)	return true;
 
 		return false;
 	}
