a745c7d0da42783090a7b74fb521a70dde401991
Ioana Marin <61522769+IoanaMarin1106@users.noreply.github.com>
2020-03-07 23:05:17 +0200

    Update Rook.java

--- Java code/Rook.java
+++ Java code/Rook.java
@@ -3,19 +3,19 @@ public final class Rook extends Piece {
 		/* Se duce de sus in jos  */
 		if (src > dest) {
 			while (src != dest) {
-				if ((src >> 8) & allPieces != 0) {
+				if ((src >> 8L) & allPieces != 0) {
 					return true;
 				}
-				src = src >> 8;
+				src = src >> 8L;
 			}
 
 		} else if (src < dest) {
 			/* Altfel se duce in jos scz pt commurile prost puse */
 			while (src != dest) {
-				if ((src << 8) & allPieces != 0) {
+				if ((src << 8L) & allPieces != 0) {
 					return true;
 				}
-				src = src << 8;
+				src = src << 8L;
 			}
 		}
 		return false;
@@ -25,19 +25,19 @@ public final class Rook extends Piece {
 		/* Ar trebui sa se duca in dreapta  */
 		if(src > dest) {
 			while (src != dest) {
-				if ((src << 1) & allPieces != 0) {
+				if ((src << 1L) & allPieces != 0) {
 					return true;
 				}
-				src = src << 1;
+				src = src << 1L;
 			}
 
 		} else if (src < dest) {
 			/* Ar trebui sa se duca in stanga */
 			while (src != dest) {
-				if ((src >> 1) & allPieces != 0) {
+				if ((src >> 1L) & allPieces != 0) {
 					return true;
 				}
-				src = src >> 1;
+				src = src >> 1L;
 			}
 		}
 		return false;
