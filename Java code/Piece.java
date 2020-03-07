3392a460f5e075c39c3e0d1119f313e2e61f5abb
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-03-08 00:19:54 +0200

    Update Piece.java

--- Java code/Piece.java
+++ Java code/Piece.java
@@ -1,11 +1,21 @@
 public abstract class Piece {
 	enum PieceType {
-		KING,
-		QUEEN,
-		ROOK,
-		BISHOP,
-		KNIGHT,
-		PAWN
+		KING(0),
+		QUEEN(1),
+		ROOK(2),
+		BISHOP(3),
+		KNIGHT(4),
+		PAWN(5);
+
+		private int index;
+
+		PieceType(int index) {
+			this.index = index;
+		}
+
+		public int getIndex() {
+			return index;
+		}
 	}
 
 	enum PieceColor {
