e8478f83d939c1b2753359dee619ad3c82e39776
Ioana Marin <61522769+IoanaMarin1106@users.noreply.github.com>
2020-03-07 23:07:59 +0200

    Update King.java

--- Java code/King.java
+++ Java code/King.java
@@ -2,12 +2,12 @@ public final class King extends Piece {
 	public static boolean isValidMove(long[] move) {
 		long src = move[0], dest = move[1];
 		
-		if(src << 9 == dest) return true;
-		if(src << 8 == dest) return true;
-		if(src << 7 == dest) return true;
-		if(src >> 9 == dest) return true;
-		if(src >> 8 == dest) return true;
-		if(src >> 7 == dest) return true;
+		if(src << 9L == dest) return true;
+		if(src << 8L == dest) return true;
+		if(src << 7L == dest) return true;
+		if(src >> 9L == dest) return true;
+		if(src >> 8L == dest) return true;
+		if(src >> 7L == dest) return true;
 		
 		return false;
 	}
