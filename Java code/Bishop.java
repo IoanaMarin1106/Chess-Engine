79e3c61101c59f34ae281d8959143607aefe8bad
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-07 21:51:06 +0200

    Update Bishop's isValidMove method

--- Java code/Bishop.java
+++ Java code/Bishop.java
@@ -1,10 +1,17 @@
 public final class Bishop extends Piece {
-	public static boolean isValidWhiteMove(long[] move, long[] whitePieces, long[] blackPieces) {
-		return true;
-
-	}
-
-	public static boolean isValidBlackMove(long[] move, long[] whitePieces, long[] blackPieces) {
-		return true;
+	public static boolean isValidMove(long[] move) {
+		long src = move[0], dest = move[1];
+		
+		for(int i = 1; i <= 8; i++) {
+			if(src << ((8*i) + i) == dest || src >> ((8*i) + i) == dest ||
+				src << ((8*i) - i) == dest || src >> ((8*i) - i) == dest ) {
+				return true;
+			}	
+			if(src << ((8*i) + i) < (1 << 64) && src >> (8*i) + i > 0) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
