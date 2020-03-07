89f03d5551fcd39a0aebb3cf1d37b2f938c7b34c
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-03-07 21:36:44 +0200

    Update Pawn.java

--- Java code/Pawn.java
+++ Java code/Pawn.java
@@ -1,10 +1,37 @@
 public final class Pawn extends Piece {
-	public static boolean isValidWhiteMove(long[] move, long[] whitePieces, long[] blackPieces) {
-		return true;
+	public static boolean isValidWhiteMove(long[] move, long blackPieces) {
+		long src = move[0], dest = move[1];
 
+		if((dest & blackPieces) != 0) {// attack
+			if((src << 7) == dest || (src << 9) == dest) {
+				return true;
+			}
+		} else {// avans
+			if(dest == (src << 8)) {
+				return true;
+			} else if(dest == (src << 16) && (src >= (1 << 8) && src < (1 << 16))) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
-	public static boolean isValidBlackMove(long[] move, long[] whitePieces, long[] blackPieces) {
-		return true;
+	public static boolean isValidBlackMove(long[] move, long whitePieces) {
+		long src = move[0], dest = move[1];
+
+		if((dest & blackPieces) != 0) {
+			if((src >> 7) == dest || (src >> 9) == dest) {
+				return true;
+			}
+		} else {
+			if(dest == (src >> 8)) {
+				return true;
+			} else if(dest == (src >> 16) && (src >= (1 << 48) && src < (1 << 56))) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
