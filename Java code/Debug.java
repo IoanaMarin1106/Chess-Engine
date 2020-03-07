787e9ede90a7ac3922ecea4b302f0ac5ff378160
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-08 01:48:19 +0200

    Update Debug.java

--- Java code/Debug.java
+++ Java code/Debug.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 public class Debug {
-	public static void main(String[] args) {
-		Game game = new Game();
-
+	
+	public static void testValidMoves(Game game) {
 		long[] move;	
 		
 		System.out.println("PAWN--------------------------------");
@@ -66,6 +65,65 @@ public class Debug {
 		move = Move.convertMove("h1f3");
 		System.out.println(Queen.isValidMove(move, 0));
 		move = Move.convertMove("h1g3");
-		System.out.println(Queen.isValidMove(move, 0));
+		System.out.println(Queen.isValidMove(move, 0));	
+	}
+	
+	public static void displayBoard (Game game) {
+		Bitboard bitboard = game.getBitboard();
+		
+		char[] board = new char[64];
+		long[] whitePieces = bitboard.getWhitePieces();
+		long[] blackPieces = bitboard.getBlackPieces();
+		
+		
+		for(int i = 0; i < 64; i++) {
+			board[i] = '-';
+			
+			if((whitePieces[0] & (1L<<i)) != 0)	board[i]='K';
+			if((whitePieces[1] & (1L<<i)) != 0)	board[i]='Q';
+			if((whitePieces[2] & (1L<<i)) != 0)	board[i]='R';
+			if((whitePieces[3] & (1L<<i)) != 0)	board[i]='B';
+			if((whitePieces[4] & (1L<<i)) != 0)	board[i]='N';
+			if((whitePieces[5] & (1L<<i)) != 0)	board[i]='P';
+			
+			if((blackPieces[0] & (1L<<i)) != 0)	board[i]='k';
+			if((blackPieces[1] & (1L<<i)) != 0)	board[i]='q';
+			if((blackPieces[2] & (1L<<i)) != 0)	board[i]='r';
+			if((blackPieces[3] & (1L<<i)) != 0)	board[i]='b';
+			if((blackPieces[4] & (1L<<i)) != 0)	board[i]='n';
+			if((blackPieces[5] & (1L<<i)) != 0)	board[i]='p';
+		}
+		
+		System.out.println("  A B C D E F G H");
+		
+		for(int i = 7; i >= 0; i--) {
+			System.out.print(i+1 + " ");
+			
+			for(int j = 0; j < 8; j++) {
+				System.out.print(board[8*i + j] + " ");
+			}
+			
+			System.out.println();
+		}
+	}
+	 
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.newCommand();
+		game.moveCommand("a2a3");
+		game.moveCommand("f2f4");
+		game.moveCommand("a4a5");
+		game.moveCommand("b2b5");
+		game.moveCommand("f4f5");
+		game.moveCommand("f5f6");
+		game.moveCommand("f6e7");
+		game.moveCommand("d1b3");
+		game.moveCommand("h1h3");
+		game.moveCommand("g1h3");
+		game.moveCommand("h3g5");
+		game.moveCommand("g5h7");
+		
+//		testValidMoves(game);
+		displayBoard(game);
 	}
 }
