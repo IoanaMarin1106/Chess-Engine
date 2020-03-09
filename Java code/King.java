f04cc90ad12c6165f4baf9919d14a2d653964d03
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-09 16:04:38 +0200

    Added generateMove

--- Java code/King.java
+++ Java code/King.java
@@ -1,14 +1,33 @@
+import java.util.ArrayList;
+
 public final class King extends Piece {
 	public static boolean isValidMove(long[] move) {
 		long src = move[0], dest = move[1];
 		
-		if(src << 9L == dest) return true;
-		if(src << 8L == dest) return true;
-		if(src << 7L == dest) return true;
-		if(src >> 9L == dest) return true;
-		if(src >> 8L == dest) return true;
-		if(src >> 7L == dest) return true;
+		if(src << 9 == dest && (src & Bitboard.FILES[0]) == 0) return true;
+		if(src << 8 == dest) return true;
+		if(src << 7 == dest && (src & Bitboard.FILES[7]) == 0) return true;
+		if(src >> 9 == dest && (src & Bitboard.FILES[7]) == 0) return true;
+		if(src >> 8 == dest) return true;
+		if(src >> 7 == dest && (src & Bitboard.FILES[0]) == 0) return true;
+		if(src << 1 == dest) return true;
+		if(src >> 1 == dest) return true;
 		
 		return false;
+	}
+	
+	public static ArrayList<long[]> generateMoves(long src) {
+		ArrayList<long[]> moves = new ArrayList<long[]>();
+		
+		moves.add(new long[] {src, (src << 9)});
+		moves.add(new long[] {src, (src << 8)});
+		moves.add(new long[] {src, (src << 7)});
+		moves.add(new long[] {src, (src >> 9)});
+		moves.add(new long[] {src, (src >> 8)});
+		moves.add(new long[] {src, (src >> 7)});
+		moves.add(new long[] {src, (src << 1)});
+		moves.add(new long[] {src, (src >> 1)});
+		
+		return moves;
 	}
 }
