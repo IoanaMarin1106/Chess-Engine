35dfdf57d9e57ee0ace29b9a0a2f6246637bc1de
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-09 16:10:03 +0200

    generateMoves

--- Java code/Queen.java
+++ Java code/Queen.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public final class Queen extends Piece {
 	public static boolean isValidMove(long[] move, long allPieces) {
 		return (Rook.isValidMove(move, allPieces) || Bishop.isValidMove(move, allPieces));
+	}
+	
+	public static ArrayList<long[]> generateMoves(long src) {
+		ArrayList<long[]> moves = new ArrayList<long[]>();
+		
+		moves.addAll(Rook.generateMoves(src));
+		moves.addAll(Bishop.generateMoves(src));
+		
+		return moves;
 	}
 }
