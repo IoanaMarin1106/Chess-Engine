da372770b983368eea29bbff79a3408b3121dcfa
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-09 17:31:10 +0200

    Update Knight.java

--- Java code/Knight.java
+++ Java code/Knight.java
@@ -1,16 +1,78 @@
+import java.util.ArrayList;
+
 public final class Knight extends Piece {
 	public static boolean isValidMove(long[] move) {
 		long src = move[0], dest = move[1];
 
-		if((src << 6L) == dest)		return true;
-		if((src << 10L) == dest)	return true;
-		if((src << 15L) == dest) 	return true;
-		if((src << 17L) == dest)	return true;
-		if((src >> 6L) == dest)		return true;
-		if((src >> 10L) == dest)	return true;
-		if((src >> 15L) == dest)	return true;
-		if((src >> 17L) == dest)	return true;
+		if((src & Bitboard.FILES[7]) == 0) {
+			if((src << 17) == dest &&
+				(src & Bitboard.RANKS[6]) == 0 &&
+				(src & Bitboard.RANKS[7]) == 0)
+				return true;
+			if((src >> 15) == dest &&
+				(src & Bitboard.RANKS[0]) == 0 && 
+				(src & Bitboard.RANKS[1]) == 0)
+				return true;
+		}
+		
+		if((src & Bitboard.FILES[0]) == 0) {
+			if((src << 15) == dest &&
+				(src & Bitboard.RANKS[6]) == 0 &&
+				(src & Bitboard.RANKS[7]) == 0)
+				return true;
+			if((src >> 17) == dest &&
+				(src & Bitboard.RANKS[0]) == 0 &&
+				(src & Bitboard.RANKS[1]) == 0)
+				return true;
+		}
+		
+		if((src & Bitboard.RANKS[0]) == 0) {
+			if((src >> 6) == dest &&
+				(src & Bitboard.FILES[7]) == 0 &&
+				(src & Bitboard.FILES[6]) == 0)
+				return true;
+			if((src >> 10) == dest &&
+				(src & Bitboard.FILES[1]) == 0 && 
+				(src & Bitboard.FILES[0]) == 0)
+				return true;
+		}
+		
+		if((src & Bitboard.RANKS[7]) == 0) {
+			if((src << 10) == dest &&
+				(src & Bitboard.FILES[7]) == 0 &&
+				(src & Bitboard.FILES[6]) == 0)
+				return true;
+			if((src << 6) == dest &&
+				(src & Bitboard.FILES[1]) == 0 &&
+				(src & Bitboard.FILES[0]) == 0)
+				return true;
+		}
 
 		return false;
+	}
+
+	public static ArrayList<long[]> generateMoves(long pieces) {
+		ArrayList<long[]> moves = new ArrayList<long[]>();
+
+		while (pieces != 0) {
+			long src = 1;
+
+			while ((src & pieces) == 0) {
+				src = (src << 1);
+			}
+
+			pieces = (pieces & (~src));
+
+			moves.add(new long[] { src, (src << 6) });
+			moves.add(new long[] { src, (src << 10) });
+			moves.add(new long[] { src, (src << 15) });
+			moves.add(new long[] { src, (src << 17) });
+			moves.add(new long[] { src, (src >> 6) });
+			moves.add(new long[] { src, (src >> 10) });
+			moves.add(new long[] { src, (src >> 15) });
+			moves.add(new long[] { src, (src >> 17) });
+		}
+
+		return moves;
 	}
 }
