4fb75b3a99bcb7e324108dc6003ad37d66ddc38b
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-03-09 15:44:11 +0200

    Update Pawn.java

--- Java code/Pawn.java
+++ Java code/Pawn.java
@@ -1,37 +1,73 @@
+import java.util.*;
+
 public final class Pawn extends Piece {
-	public static boolean isValidWhiteMove(long[] move, long blackPieces) {
+
+	public static boolean isValidWhiteMove(long[] move, long blackPieces, long allPieces) {
 		long src = move[0], dest = move[1];
 
 		if((dest & blackPieces) != 0) {// attack
-			if((src << 7L) == dest || (src << 9L) == dest) {
+			if((src << 7L) == dest && (src & Bitboard.FILES[7]) == 0) {
+				return true;
+			}
+
+			if((src << 9L) == dest && (src & Bitboard.FILES[0]) == 0) {
 				return true;
 			}
 		} else {// avans
 			if(dest == (src << 8L)) {
 				return true;
 			} else if(dest == (src << 16L) && (src >= (1L << 8L) && src < (1L << 16L))) {
-				return true;
+				if(((src << 8L) & allPieces) == 0) {
+					return true;
+				}
 			}
 		}
 
 		return false;
 	}
 
-	public static boolean isValidBlackMove(long[] move, long whitePieces) {
+	public static boolean isValidBlackMove(long[] move, long whitePieces, long allPieces) {
 		long src = move[0], dest = move[1];
 
 		if((dest & whitePieces) != 0) {
-			if((src >> 7L) == dest || (src >> 9L) == dest) {
+			if((src >> 7L) == dest && (src & Bitboard.FILES[0]) == 0) {
+				return true;
+			}
+
+			if((src >> 9L) == dest && (src & Bitboard.FILES[7]) == 0) {
 				return true;
 			}
 		} else {
 			if(dest == (src >> 8L)) {
 				return true;
 			} else if(dest == (src >> 16L) && (src >= (1L << 48L) && src < (1L << 56L))) {
-				return true;
+				if(((src >> 8L) & allPieces) == 0) {
+					return true;
+				}
 			}
 		}
 
 		return false;
+	}
+
+	public static ArrayList<long[]> generateMoves(long pieces) {
+		ArrayList<long[]> moves = new ArrayList<long[]>();
+
+		while(pieces != 0) {
+			long src = 1;
+
+			while((src & pieces) == 0) {
+				src = (src << 1);
+			}
+
+			pieces = (pieces & (~src)); // sterg bitul asta (aka pionul asta)
+
+			moves.add(new long[] {src, (src >> 8)});	// single push
+			moves.add(new long[] {src, (src >> 16)});	// double push
+			moves.add(new long[] {src, (src >> 7)});	// attack
+			moves.add(new long[] {src, (src >> 9)});	// attack
+		}
+
+		return moves;
 	}
 }
