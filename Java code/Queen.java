68b1fbb9b5460ff205f97481c25f25ca62ae925f
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-03-09 23:49:48 +0200

    Update Queen.java

--- Java code/Queen.java
+++ Java code/Queen.java
@@ -4,13 +4,11 @@ public final class Queen extends Piece {
 	public static boolean isValidMove(long[] move, long allPieces) {
 		return (Rook.isValidMove(move, allPieces) || Bishop.isValidMove(move, allPieces));
 	}
-	
-	public static ArrayList<long[]> generateMoves(long src) {
-		ArrayList<long[]> moves = new ArrayList<long[]>();
-		
-		moves.addAll(Rook.generateMoves(src));
-		moves.addAll(Bishop.generateMoves(src));
-		
+
+	public static ArrayList<long[]> generateMoves(long pieces) {
+		ArrayList<long[]> moves = Rook.generateMoves(pieces);
+		moves.addAll(Bishop.generateMoves(pieces));
+
 		return moves;
 	}
 }
