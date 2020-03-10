e325f021e03cab6ad42c116fb0f8e1e252885487
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-03-11 00:29:00 +0200

    Update Piece.java

--- Java code/Piece.java
+++ Java code/Piece.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public abstract class Piece {
-	enum PieceType {
+	enum Type {
 		KING(0),
 		QUEEN(1),
 		ROOK(2),
@@ -7,9 +9,9 @@ public abstract class Piece {
 		KNIGHT(4),
 		PAWN(5);
 
-		private int index;
+		public int index;
 
-		PieceType(int index) {
+		Type(int index) {
 			this.index = index;
 		}
 
@@ -18,8 +20,68 @@ public abstract class Piece {
 		}
 	}
 
-	enum PieceColor {
+	enum Color {
 		WHITE,
 		BLACK
+	}
+
+	public static Type getType(int index) {
+		if(index == 0)	return Type.KING;
+		if(index == 1)	return Type.QUEEN;
+		if(index == 2)	return Type.ROOK;
+		if(index == 3)	return Type.BISHOP;
+		if(index == 4)	return Type.KNIGHT;
+		if(index == 5)	return Type.PAWN;
+
+		return null;
+	}
+
+	public static boolean isValidMove(
+		Type type, Color color,
+		long[] move,
+		long color1Pieces, long color2Pieces
+		) {
+
+		switch(type) {
+			case KING:
+				return King.isValidMove(move);
+			case QUEEN:
+				return Queen.isValidMove(move, (color1Pieces | color2Pieces));
+			case ROOK:
+				return Rook.isValidMove(move, (color1Pieces | color2Pieces));
+			case BISHOP:
+				return Bishop.isValidMove(move, (color1Pieces | color2Pieces));
+			case KNIGHT:
+				return Knight.isValidMove(move);
+			case PAWN:
+				if(color == Piece.Color.WHITE) {
+					return Pawn.isValidWhiteMove(move, color2Pieces,
+											(color1Pieces | color2Pieces));
+				} else {
+					return Pawn.isValidBlackMove(move, color2Pieces,
+											(color1Pieces | color2Pieces));
+				}
+			default:
+				return false;
+		}
+	}
+
+	public static ArrayList<long[]> generateMoves(Type type, long pieces) {
+		switch(type) {
+			case KING:
+				return King.generateMoves(pieces);
+			case QUEEN:
+				return Queen.generateMoves(pieces);
+			case ROOK:
+				return Rook.generateMoves(pieces);
+			case BISHOP:
+				return Bishop.generateMoves(pieces);
+			case KNIGHT:
+				return Knight.generateMoves(pieces);
+			case PAWN:
+				return Pawn.generateMoves(pieces);
+			default:
+				return null;
+		}
 	}
 }
