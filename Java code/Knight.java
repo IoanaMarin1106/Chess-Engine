0fcb60f47018de327a0a59dc817b9616608f4ac2
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-12 22:59:32 +0200

    Update Knight.java

--- Java code/Knight.java
+++ Java code/Knight.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 /**
  * This class stands for the Knight piece in the chess game.
+ *
  * It has two static methods: one that checks if a move
  * is valid and one that generates all the moves possible.
  * 
