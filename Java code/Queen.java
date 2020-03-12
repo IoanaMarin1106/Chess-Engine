7df925905256cab3413211b254a7cdf51c640a00
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-12 21:37:11 +0200

    added comments

--- Java code/Queen.java
+++ Java code/Queen.java
@@ -1,13 +1,34 @@
 import java.util.ArrayList;
 
+/**
+ * This class stands for the Queen piece in the chess game.
+ * It has two static methods: one that checks if a move
+ * is valid, and one that generates all the moves possible.
+ * 
+ * @author Creierul si Cerebelii
+ *
+ */
 public final class Queen extends Piece {
+	/**
+	 * Method which checks if a move is valid. This method
+	 * returns true if the move is valid for the Queen piece, 
+	 * for instance, it will return false if a player tries to 
+	 * move the Queen as a Knight.
+	 * @param move the move to be checked.
+	 * @param allPieces the actual state of every piece on the board
+	 * @return true for valid move, false otherwise.
+	 */
 	public static boolean isValidMove(long[] move, long allPieces) {
 		return (Rook.isValidMove(move, allPieces) || Bishop.isValidMove(move, allPieces));
 	}
 
-	public static ArrayList<long[]> generateMoves(long pieces) {
-		ArrayList<long[]> moves = Rook.generateMoves(pieces);
-		moves.addAll(Bishop.generateMoves(pieces));
+	/**
+	 * @param piece the position of the Queen.
+	 * @return an array with all moves possible for the Queen.
+	 */
+	public static ArrayList<long[]> generateMoves(long piece) {
+		ArrayList<long[]> moves = Rook.generateMoves(piece);
+		moves.addAll(Bishop.generateMoves(piece));
 
 		return moves;
 	}
