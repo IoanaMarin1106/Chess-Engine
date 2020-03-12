69155269a5c730c056062bb24f4d1ff5b61d4b04
Ioana Marin <61522769+IoanaMarin1106@users.noreply.github.com>
2020-03-12 23:07:05 +0200

    Update Pawn.java

--- Java code/Pawn.java
+++ Java code/Pawn.java
@@ -1,11 +1,38 @@
 import java.util.*;
 
+/**
+ * This class stands for the Pawn piece in the chess game.
+ * 
+ * It has three static methods: one that checks if a move
+ * is valid for white color(this means that it can only move
+ * to the top of board), one that checks if a move is valid for
+ * black color(this means that it cand only move to the bottom of
+ * the board) and one that generates all the moves possible.
+ *
+ *	@author Creierul si Cerebelii
+ *
+ */
 public final class Pawn extends Piece {
 
+	/**
+	 * Method which checks if a move is valid for white color, this
+	 * means that it can only move to the top of the board.
+	 * Except on its first move, a pawn may only move one square 
+	 * forward at a time. Pawns can never move backwards. Although 
+	 * a pawn moves straight forward it captures by talking one 
+	 * square diagonally forward.
+	 * @param move the move to be checked.
+	 * @param blackPieces the current state of the black pieces positions.
+	 * @param allPieces the current state of the board.
+	 * @return true for valid move, false otherwise. 
+	 */
 	public static boolean isValidWhiteMove(long[] move, long blackPieces, long allPieces) {
 		long src = move[0], dest = move[1];
 
-		if((dest & blackPieces) != 0) {// attack
+		/**
+		* The Pawn is on attack.
+		*/
+		if((dest & blackPieces) != 0) {
 			if((src << 7L) == dest && (src & Bitboard.FILES[7]) == 0) {
 				return true;
 			}
@@ -13,7 +40,10 @@ public final class Pawn extends Piece {
 			if((src << 9L) == dest && (src & Bitboard.FILES[0]) == 0) {
 				return true;
 			}
-		} else {// avans
+		} else {
+			/**
+			* The Pawn is pushing forward.
+			*/
 			if(dest == (src << 8L) && (src & Bitboard.RANKS[7]) == 0) {
 				return true;
 			} else if(dest == (src << 16L) && (src >= (1L << 8L) && src < (1L << 16L))) {
@@ -26,9 +56,24 @@ public final class Pawn extends Piece {
 		return false;
 	}
 
+	/**
+	 * Method which checks if a move is valid for black color, this
+	 * means that it can only move to the bottom of the board.
+	 * Except on its first move, a pawn may only move one square 
+	 * forward at a time. Pawns can never move backwards. Although 
+	 * a pawn moves straight forward it captures by talking one 
+	 * square diagonally forward.
+	 * @param move the move to be checked.
+	 * @param whitePieces the current state of the white pieces positions.
+	 * @param allPieces the current state of the board.
+	 * @return true for valid move, false otherwise. 
+	 */
 	public static boolean isValidBlackMove(long[] move, long whitePieces, long allPieces) {
 		long src = move[0], dest = move[1];
 
+		/**
+		* The Pawn is on attack.
+		*/
 		if((dest & whitePieces) != 0) {
 			if((src >> 7L) == dest && (src & Bitboard.FILES[0]) == 0) {
 				return true;
@@ -38,6 +83,9 @@ public final class Pawn extends Piece {
 				return true;
 			}
 		} else {
+			/**
+			* The Pawn is pushing forward.
+			*/
 			if(dest == (src >> 8L) && (src & Bitboard.RANKS[0]) == 0) {
 				return true;
 			} else if(dest == (src >> 16L) && (src >= (1L << 48L) && src < (1L << 56L))) {
@@ -50,6 +98,12 @@ public final class Pawn extends Piece {
 		return false;
 	}
 
+	/**
+	 *	Method which generates an ArrayList of possible moves for
+	 *	the Pawn piece. It only generates valid moves.
+	 *	@param pieces only the positions on which the Pawns are on the board.
+	 *	@return an array with all moves possible for the Pawns.
+	 */
 	public static ArrayList<long[]> generateMoves(long pieces) {
 		ArrayList<long[]> moves = new ArrayList<long[]>();
 
@@ -60,12 +114,12 @@ public final class Pawn extends Piece {
 				src = (src << 1);
 			}
 
-			pieces = (pieces & (~src)); // sterg bitul asta (aka pionul asta)
+			pieces = (pieces & (~src));
 
-			moves.add(new long[] {src, (src >> 8)});	// single push
-			moves.add(new long[] {src, (src >> 16)});	// double push
-			moves.add(new long[] {src, (src >> 7)});	// attack
-			moves.add(new long[] {src, (src >> 9)});	// attack
+			moves.add(new long[] {src, (src >> 8)});
+			moves.add(new long[] {src, (src >> 16)});
+			moves.add(new long[] {src, (src >> 7)});	
+			moves.add(new long[] {src, (src >> 9)});	
 			moves.add(new long[] {src, (src << 8)});
 			moves.add(new long[] {src, (src << 16)});
 			moves.add(new long[] {src, (src << 7)});
@@ -75,3 +129,4 @@ public final class Pawn extends Piece {
 		return moves;
 	}
 }
+
