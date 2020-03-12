5daae98c74db845d6e10b7a8cca5380b70cfef3a
Alexandru Ghiculescu <52600189+ghiculescualexandru@users.noreply.github.com>
2020-03-12 20:39:09 +0200

    added comments

--- Java code/Debug.java
+++ Java code/Debug.java
@@ -1,80 +1,24 @@
-import java.util.Scanner;
-
+/**
+ * Class designed for debugging, which mainly has a 
+ * method to display the board in the console/terminal
+ * and also has some methods for testing.
+ * 
+ * @author
+ *
+ */
 public class Debug {
-	
-	public static void testValidMoves(Game game) {
-		long[] move;	
-		
-		System.out.println("PAWN--------------------------------");
-		// move = Move.convertMove("c2c4");
-		// System.out.println(Pawn.isValidWhiteMove(move, 0));
-		// move = Move.convertMove("c2c3");
-		// System.out.println(Pawn.isValidWhiteMove(move, 0));
-		// move = Move.convertMove("a7a5");
-		// System.out.println(Pawn.isValidBlackMove(move, 0));
-		// move = Move.convertMove("e5e4");
-		// System.out.println(Pawn.isValidBlackMove(move, 0));
-		
-		// move = Move.convertMove("c3d3");
-		// System.out.println(Pawn.isValidBlackMove(move, 0));
-		// System.out.println(Pawn.isValidWhiteMove(move, 0));
-		
-		System.out.println("BISHOP--------------------------------");
-		
-		move = Move.convertMove("g2b7");
-		System.out.println(Bishop.isValidMove(move, 0));
-		move = Move.convertMove("g5c1");
-		System.out.println(Bishop.isValidMove(move, 0));
-		
-		move = Move.convertMove("h1h4");
-		System.out.println(Bishop.isValidMove(move, 0));
-		move = Move.convertMove("h1h3");
-		System.out.println(Bishop.isValidMove(move, 0));
-		
-		
-		System.out.println("KING--------------------------------");
-		
-		move = Move.convertMove("g1g2");
-		System.out.println(King.isValidMove(move));
-		move = Move.convertMove("g5h6");
-		System.out.println(King.isValidMove(move));
-		move = Move.convertMove("h8h6");
-		System.out.println(King.isValidMove(move));
-	
-		System.out.println("KNIGHT----------------------------");
-		move = Move.convertMove("g1f3");
-		System.out.println(Knight.isValidMove(move));
-		move = Move.convertMove("h6f7");
-		System.out.println(Knight.isValidMove(move));
-		move = Move.convertMove("g6f5");
-		System.out.println(Knight.isValidMove(move));
-	
-		System.out.println("ROOK---------------------------");
-		move = Move.convertMove("h1h2");
-		System.out.println(Rook.isValidMove(move, 0));
-		move = Move.convertMove("h4a4");
-		System.out.println(Rook.isValidMove(move, 0));
-		move = Move.convertMove("h1g7");
-		System.out.println(Rook.isValidMove(move, 0));
-	
-		System.out.println("QUEEN-------------------------");
-		move = Move.convertMove("h1h2");
-		System.out.println(Queen.isValidMove(move, 0));
-		move = Move.convertMove("h4a4");
-		System.out.println(Queen.isValidMove(move, 0));
-		move = Move.convertMove("h1f3");
-		System.out.println(Queen.isValidMove(move, 0));
-		move = Move.convertMove("h1g3");
-		System.out.println(Queen.isValidMove(move, 0));	
-	}
-	
+
+	/**
+	 * Method that displays the actual state of the board
+	 * in the console/terminal.
+	 * @param game the game with its actual state.
+	 */
 	public static void displayBoard (Game game) {
 		Bitboard bitboard = game.getBitboard();
 		
 		char[] board = new char[64];
 		long[] whitePieces = bitboard.getWhitePieces();
 		long[] blackPieces = bitboard.getBlackPieces();
-		
 		
 		for(int i = 0; i < 64; i++) {
 			board[i] = '-';
@@ -106,28 +50,73 @@ public class Debug {
 			
 			System.out.println();
 		}
-	}
+	}	
 	
-	public static void testDisplayBoard(Game game) {
-		// game.newCommand();
-		// game.moveCommand("a2a3");
-		// game.moveCommand("f2f4");
-		// game.moveCommand("a4a5");
-		// game.moveCommand("b2b5");
-		// game.moveCommand("f4f5");
-		// game.moveCommand("f5f6");
-		// game.moveCommand("f6e7");
-		// game.moveCommand("d1b3");
-		// game.moveCommand("h1h3");
-		// game.moveCommand("g1h3");
-		// game.moveCommand("h3g5");
-		// game.moveCommand("g5h7");
+	/**
+	 * Method that tests valid moves for every type
+	 * of piece in the chess game.
+	 * @param game the game with its actual state.
+	 */
+	public static void testValidMoves(Game game) {
+		long[] move;
+
+		System.out.println("PAWN--------------------------------");
+		move = Move.convertMove("c2c4");
+		System.out.println(Pawn.isValidWhiteMove(move, 0, 0));
+		move = Move.convertMove("c2c3");
+		System.out.println(Pawn.isValidWhiteMove(move, 0, 0));
+		move = Move.convertMove("a7a5");
+		System.out.println(Pawn.isValidBlackMove(move, 0, 0));
+		move = Move.convertMove("e5e4");
+		System.out.println(Pawn.isValidBlackMove(move, 0, 0));
+		move = Move.convertMove("c3d3");
+
+		System.out.println(Pawn.isValidBlackMove(move, 0, 0));
+		System.out.println(Pawn.isValidWhiteMove(move, 0, 0));
+
+		System.out.println("BISHOP--------------------------------");
+		move = Move.convertMove("g2b7");
+		System.out.println(Bishop.isValidMove(move, 0));
+		move = Move.convertMove("g5c1");
+		System.out.println(Bishop.isValidMove(move, 0));
+
+		move = Move.convertMove("h1h4");
+		System.out.println(Bishop.isValidMove(move, 0));
+		move = Move.convertMove("h1h3");
+		System.out.println(Bishop.isValidMove(move, 0));
+
+		System.out.println("KING--------------------------------");
+		move = Move.convertMove("g1g2");
+		System.out.println(King.isValidMove(move));
+		move = Move.convertMove("g5h6");
+		System.out.println(King.isValidMove(move));
+		move = Move.convertMove("h8h6");
+		System.out.println(King.isValidMove(move));
+
+		System.out.println("KNIGHT----------------------------");
+		move = Move.convertMove("g1f3");
+		System.out.println(Knight.isValidMove(move));
+		move = Move.convertMove("h6f7");
+		System.out.println(Knight.isValidMove(move));
+		move = Move.convertMove("g6f5");
+		System.out.println(Knight.isValidMove(move));
+
+		System.out.println("ROOK---------------------------");
+		move = Move.convertMove("h1h2");
+		System.out.println(Rook.isValidMove(move, 0));
+		move = Move.convertMove("h4a4");
+		System.out.println(Rook.isValidMove(move, 0));
+		move = Move.convertMove("h1g7");
+		System.out.println(Rook.isValidMove(move, 0));
+
+		System.out.println("QUEEN-------------------------");
+		move = Move.convertMove("h1h2");
+		System.out.println(Queen.isValidMove(move, 0));
+		move = Move.convertMove("h4a4");
+		System.out.println(Queen.isValidMove(move, 0));
+		move = Move.convertMove("h1f3");
+		System.out.println(Queen.isValidMove(move, 0));
+		move = Move.convertMove("h1g3");
+		System.out.println(Queen.isValidMove(move, 0));
 	}
-	 
-// 	public static void main(String[] args) {
-// 		Game game = new Game();
-// //		testDisplayBoard(game);
-// //		testValidMoves(game);
-// 		displayBoard(game);
-// 	}
 }
