d4c13a1dfbc417b7c1e4bd769482f1ae49013a0a
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-04-04 17:09:52 +0300

    Update King.java

--- Java code/King.java
+++ Java code/King.java
@@ -18,9 +18,7 @@ public final class King extends Piece {
 	 * @param move the move to be checked.
 	 * @return true for valid move, false otherwise.
 	 */
-	public static boolean isValidMove(long[] move) {
-		long src = move[0], dest = move[1];
-		
+	public static boolean isValidMove(long src, long dest) {		
 		if(src << 1 == dest && (src & Bitboard.FILES[7]) == 0) return true;
 		if(src >>> 1 == dest && (src & Bitboard.FILES[0]) == 0) return true;
 
@@ -45,18 +43,45 @@ public final class King extends Piece {
 	 * @param src the source of the move.
 	 * @return an array with all moves possible for the King.
 	 */
-	public static ArrayList<long[]> generateMoves(long src) {
-		ArrayList<long[]> moves = new ArrayList<long[]>();
+	// public static ArrayList<long[]> generateMoves(long src) {
+	// 	ArrayList<long[]> moves = new ArrayList<long[]>();
 		
-		moves.add(new long[] {src, (src << 9)});
-		moves.add(new long[] {src, (src << 8)});
-		moves.add(new long[] {src, (src << 7)});
-		moves.add(new long[] {src, (src >>> 9)});
-		moves.add(new long[] {src, (src >>> 8)});
-		moves.add(new long[] {src, (src >>> 7)});
-		moves.add(new long[] {src, (src << 1)});
-		moves.add(new long[] {src, (src >>> 1)});
+	// 	moves.add(new long[] {src, (src << 9)});
+	// 	moves.add(new long[] {src, (src << 8)});
+	// 	moves.add(new long[] {src, (src << 7)});
+	// 	moves.add(new long[] {src, (src >>> 9)});
+	// 	moves.add(new long[] {src, (src >>> 8)});
+	// 	moves.add(new long[] {src, (src >>> 7)});
+	// 	moves.add(new long[] {src, (src << 1)});
+	// 	moves.add(new long[] {src, (src >>> 1)});
 		
+	// 	return moves;
+	// }
+
+	public static ArrayList<long[]> generateMoves(
+		Piece.Type type, Piece.Color color, Bitboard board
+		) {
+
+		ArrayList<long[]> moves = new ArrayList<long[]>;
+		long src, pieces;
+
+		if(color == Piece.Color.WHITE) {
+			src = board.whitePieces[0];
+			attackerPieces = board.getAllPieces(board.whitePieces);
+		} else {
+			src = board.blackPieces[0];
+			attackerPieces = board.getAllPieces(board.blackPieces);
+		}
+
+		long[] dest = {(src << 9), (src << 8), (src << 7), (src >>> 9), (src >>> 8),
+					(src >>> 7), (src << 1), (src >>> 1)};
+
+		for(int i = 0; i < dest.length; i++) {
+			if((dest[i] & attackerPieces) == 0 && isValidMove(src, dest[i])) {
+				moves.add(new long[] {src, dest[i]});
+			}
+		}
+
 		return moves;
 	}
 }
