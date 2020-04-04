fd46fc3512c17c0fab6d6ee6d05bdadcecf3fcd5
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-04-04 19:02:26 +0300

    Create Negamax.java

---
+++ Java code/Negamax.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Negamax {
+	public static int negamax(
+		Bitboard board, Piece.Color playerColor,
+		int alpha, int beta, int depth) {
+
+
+		if(depth == 0 || board.gameOver()) {
+			return Evaluation.evaluate(board, playerColor);
+		}
+
+		ArrayList<long[]> moves = board.generateAllMoves(playerColor);
+		Piece.Color vsColor = (playerColor == Piece.Color.WHITE) ?
+							Piece.Color.BLACK : Piece.Color.WHITE;
+
+		for(long[] move : moves) {
+			Bitboard newState = (Bitboard)board.clone();
+
+			newState.makeMove(move, playerColor);
+
+			if(newState.isCheck(playerColor)) {
+				continue;
+			}
+
+			int score = -negamax(newState, vsColor, -beta, -alpha, depth - 1);
+
+			if(score >= alpha) {
+				alpha = score;
+			}
+
+			if(alpha >= beta) {
+				break;
+			}
+		}
+
+		return alpha;
+	}
+
+	public static long[] negamaxHandler(Bitboard board, Piece.Color playerColor) {
+		ArrayList<long[]> moves = board.generateAllMoves(playerColor);
+		Piece.Color vsColor = (playerColor == Piece.Color.WHITE) ?
+							Piece.Color.BLACK : Piece.Color.WHITE;
+
+		long[] bestMove = null;
+		int alpha = Integer.MIN_VALUE, beta = Integer.MAX_VALUE;
+
+		for(long[] move : moves) {
+			Bitboard newState = (Bitboard)board.clone();
+			newState.makeMove(move, playerColor);
+
+			if(newState.isCheck(playerColor)) {
+				continue;
+			}
+
+			int score = -negamax(newState, vsColor, -beta, -alpha, 6);
+
+			if(score >= alpha) {
+				alpha = score;
+				bestMove = move;
+			}
+		}
+
+		return bestMove;
+	}
+}
