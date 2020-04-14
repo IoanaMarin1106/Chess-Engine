20394fb8396d3ca4e83b92939bc8189cb5b5c4a6
Roxana Stiuca <50176171+roxanastiuca@users.noreply.github.com>
2020-04-14 22:26:09 +0300

    Update Knight.java

--- Java code/Knight.java
+++ Java code/Knight.java
@@ -10,113 +10,113 @@ import java.util.ArrayList;
  *
  */
 public final class Knight extends Piece {
-	/**
-	 * Method which checks if a move is valid. This method returns true if the move
-	 * is valid for the Knight piece, for instance, it will return false if a player
-	 * tries to move in a straight line with the Knight.
-	 * 
-	 * @param move the move to be checked.
-	 * @return true for valid move, false otherwise.
-	 */
-	public static boolean isValidMove(long src, long dest) {
-		if ((src & Bitboard.FILES[0]) == 0) {
-			if ((src << 17) == dest && (src & Bitboard.RANKS[6]) == 0 && (src & Bitboard.RANKS[7]) == 0)
-				return true;
-			if ((src >>> 15) == dest && (src & Bitboard.RANKS[0]) == 0 && (src & Bitboard.RANKS[1]) == 0)
-				return true;
-		}
 
-		if ((src & Bitboard.FILES[7]) == 0) {
-			if ((src << 15) == dest && (src & Bitboard.RANKS[6]) == 0 && (src & Bitboard.RANKS[7]) == 0)
-				return true;
-			if ((src >>> 17) == dest && (src & Bitboard.RANKS[0]) == 0 && (src & Bitboard.RANKS[1]) == 0)
-				return true;
-		}
+	public static ArrayList<long[]> generateMoves(
+		Type type, Color color, long knights, long attackerPieces, long defenderPieces
+		) {
 
-		if ((src & Bitboard.RANKS[0]) == 0) {
-			if ((src >>> 6) == dest && (src & Bitboard.FILES[1]) == 0 && (src & Bitboard.FILES[0]) == 0)
-				return true;
-			if ((src >>> 10) == dest && (src & Bitboard.FILES[7]) == 0 && (src & Bitboard.FILES[6]) == 0)
-				return true;
-		}
-
-		if ((src & Bitboard.RANKS[7]) == 0) {
-			if ((src << 10) == dest && (src & Bitboard.FILES[1]) == 0 && (src & Bitboard.FILES[0]) == 0)
-				return true;
-			if ((src << 6) == dest && (src & Bitboard.FILES[7]) == 0 && (src & Bitboard.FILES[6]) == 0)
-				return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * Methods which generates an ArrayList of possible moves for the Knight piece.
-	 * It only generates valid moves.
-	 * 
-	 * @param src the source of the move.
-	 * @return an array with all moves possible for the Knight.
-	 */
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//	public static ArrayList<long[]> generateMoves(long pieces) {
-//		ArrayList<long[]> moves = new ArrayList<long[]>();
-//
-//		while (pieces != 0) {
-//			long src = 1;
-//
-//			while ((src & pieces) == 0) {
-//				src = (src << 1);
-//			}
-//
-//			pieces = (pieces & (~src));
-//
-//			moves.add(new long[] { src, (src << 6) });
-//			moves.add(new long[] { src, (src << 10) });
-//			moves.add(new long[] { src, (src << 15) });
-//			moves.add(new long[] { src, (src << 17) });
-//			moves.add(new long[] { src, (src >>> 6) });
-//			moves.add(new long[] { src, (src >>> 10) });
-//			moves.add(new long[] { src, (src >>> 15) });
-//			moves.add(new long[] { src, (src >>> 17) });
-//		}
-//
-//		return moves;
-//	}
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	public static ArrayList<long[]> generateMoves(Type type, Color color, Bitboard board) {
 		ArrayList<long[]> moves = new ArrayList<long[]>();
 
-		long src, pieces, attackerPieces;
+		long[] RANKS = Bitboard.RANKS;
+		long[] FILES = Bitboard.FILES;
 
-		if (color == Piece.Color.WHITE) {
-			pieces= board.whitePieces[4];
-			attackerPieces = board.getAllPieces(board.whitePieces);
-		} else {
-			pieces= board.blackPieces[4];
-			attackerPieces = board.getAllPieces(board.blackPieces);
-		}
+		while(knights != 0) {
+			long src = (1L << Long.numberOfTrailingZeros(knights));
+			knights &= (~src);
 
-		while (pieces != 0) {
-			src = 1;
+			long east = ((src & (~FILES[0])) << 1);
 
-			while ((src & pieces) == 0) {
-				src = (src << 1);
+			if(east != 0) {
+				long upEast = (((east & (~RANKS[7]) & (~RANKS[6])) << 16) & (~attackerPieces));
+
+				if(upEast != 0) {
+					moves.add(new long[] {src, upEast});
+				}
+
+				long downEast = (((east & (~RANKS[0]) & (~RANKS[1])) >>> 16) & (~attackerPieces));
+
+				if(downEast != 0) {
+					moves.add(new long[] {src, downEast});
+				}
+
+				east = ((east & (~FILES[0])) << 1);
+
+				if(east != 0) {
+					upEast = (((east & (~RANKS[7])) << 8) & (~attackerPieces));
+
+					if(upEast != 0) {
+						moves.add(new long[] {src, upEast});
+					}
+
+					downEast = (((east & (~RANKS[0])) >>> 8) & (~attackerPieces));
+
+					if(downEast != 0) {
+						moves.add(new long[] {src, downEast});
+					}
+				}
 			}
 
-			pieces = (pieces & (~src));
+			long west = ((src & (~FILES[7])) >>> 1);
 
-			long dest[] = {(src << 6), (src << 10), (src << 15), (src << 17), (src >>> 6), (src >>> 10), (src >>> 15),
-					(src >>> 17) };
+			if(west != 0) {
+				long upWest = (((west & (~RANKS[7]) & (~RANKS[6])) << 16) & (~attackerPieces));
 
-			for (int i = 0; i < dest.length; i++) {
-				if ((dest[i] & attackerPieces) == 0 && isValidMove(src, dest[i])) {
-					moves.add(new long[] { src, dest[i] });
+				if(upWest != 0) {
+					moves.add(new long[] {src, upWest});
+				}
+
+				long downWest = (((west & (~RANKS[0]) & (~RANKS[1])) >>> 16) & (~attackerPieces));
+
+				if(downWest != 0) {
+					moves.add(new long[] {src, downWest});
+				}
+
+				west = ((west & (~FILES[7])) >>> 1);
+
+				if(west != 0) {
+					upWest = (((west & (~RANKS[7])) << 8) & (~attackerPieces));
+
+					if(upWest != 0) {
+						moves.add(new long[] {src, upWest});
+					}
+
+					downWest = (((west & (~RANKS[0])) >>> 8) & (~attackerPieces));
+
+					if(downWest != 0) {
+						moves.add(new long[] {src, downWest});
+					}
 				}
 			}
 		}
 
 		return moves;
+	}
+
+	public static long generateMovesMap(Color color, long knights, long attackerPieces, long defenderPieces) {
+		long attacksMap = 0L;
+
+		long[] RANKS = Bitboard.RANKS;
+		long[] FILES = Bitboard.FILES;
+
+		long attacks = 0L;
+
+		long east = ((knights & (~FILES[0])) << 1);
+		long west = ((knights & (~FILES[7])) >>> 1);
+
+		attacks |= (((east | west) & (~RANKS[7]) & (~RANKS[6])) << 16);
+		attacks |= (((east | west) & (~RANKS[0]) & (~RANKS[1])) >>> 16);
+
+		east = ((east & (~FILES[0])) << 1);
+		west = ((west & (~FILES[7])) >>> 1);
+
+		attacks |= (((east | west) & (~RANKS[7])) << 8);
+		attacks |= (((east | west) & (~RANKS[0])) >>> 8);
+
+		attacksMap |= attacks;
+
+		attacksMap &= ~attackerPieces;
+
+		return attacksMap;
 	}
 
 }
